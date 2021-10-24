5d21c90a36517afca44450116b41f957a0413303
Jorge-jr <jsribeirojr@hotmail.com>
2021-10-24 16:05:44 +1100

    envio dos enderecos capturados via socket

--- sniffer2.c
+++ sniffer2.c
@@ -25,6 +25,8 @@ int sniffed_devices = 0;
 
 int main(int argc, char ** argv){
 
+
+
 	char device[] = "mon0";
 	char *errbuf;
 	int linktype;
@@ -67,6 +69,16 @@ int main(int argc, char ** argv){
 void packetHandler(u_char *args, const struct pcap_pkthdr *header, const u_char *packet){
 
 
+
+	int socket_desc;  //socket descriptor
+	socket_desc = socket(AF_INET, SOCK_STREAM, 0);  //AF_INET = IPV4  --  SOCK_STREAM = TCP
+	if(socket_desc == -1) printf("Could not create socket");
+	struct sockaddr_in server;
+	server.sin_family = AF_INET;
+	server.sin_port = htons(PORT);
+	server.sin_addr.s_addr = inet_addr("192.168.0.128");
+
+
 	struct ieee80211_radiotap_header *rthdr;
 	rthdr = (struct ieee80211_radiotap_header *) packet;
 	uint8_t *type_subtype = (uint8_t *) packet + rthdr->it_len;
@@ -79,46 +91,35 @@ void packetHandler(u_char *args, const struct pcap_pkthdr *header, const u_char 
         }else if ((*type_subtype & 12) == 0){  //type = 00 ->  management
 		struct mgmt_header_t *hdr = (struct mgmt_header_t *) (packet + rthdr->it_len);
 
-		if (devices == NULL){
-			struct device *new_dev = (struct device *) malloc (sizeof(struct device));
-
-			new_dev->address[0] = hdr->sa[0];
-			sniffed_devices++;
-			new_dev->count = 1;
-			new_dev->next = NULL;
-			devices = new_dev;
+		if (connect(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0){
+			 printf("connection error\n");
 		}else{
-			device *dev = devices;
-			while (dev != NULL){
-				for(int i=0;i<6;i++){
-					if (hdr->sa[i] != dev->address[i]){
-						if ((dev->next==NULL)){
-							struct device *new_dev = (struct device *) malloc (sizeof(struct device));
-							for (int i=0;i<6;i++){
-				                                new_dev->address[i] = hdr->sa[i];
-                        				}
+			printf("connected\n");
+			char message[6];
 
-							new_dev->next = NULL;
-							dev->next = new_dev;
-							sniffed_devices++;
-							dev = NULL;
-							i = 6;
-						}else{
-							dev = dev->next;
-							i = 0;
-						}
-					}else if ((i == 5)){
-						dev->count++;
-						dev = NULL;
-					}
-				}
+			//message[0] = hdr->sa[0];
+			//printf("%02x:%02x:%02x\n", message[0], message[1],message[2]);
+			//struct device *new_dev = (struct device *) malloc (sizeof(struct device));
+			//new_dev->address[0] = hdr->sa[0];
+			//FILE *fd = fdopen(socket_desc, "w");
+			sprintf(message,"%02x:%02x:%02x:%02x:%02x:%02x \n", hdr->sa[0],
+                	                                                    hdr->sa[1],
+                        	                                            hdr->sa[2],
+                                	                                    hdr->sa[3],
+                                        	                            hdr->sa[4],
+                                                	                    hdr->sa[5]);
+			int sockerr = send(socket_desc, message, strlen(message) , 0);
+			if (sockerr >= 0){
+
+			 	printf("%s sended\n", message);
+			}else{
+				printf("socket error %d -> %s\n", sockerr, message);
 			}
 		}
 
-
         }else if ((*type_subtype & 8) == 8){  //type = 10 -> data
 		struct data_header_t *hdr = (struct data_header_t *) (packet + rthdr->it_len);
-		if (devices == NULL){
+		/*if (devices == NULL){
 			struct device *new_dev = (struct device *) malloc (sizeof(struct device));
 			new_dev->address[0] = hdr->sa[0];
 			new_dev->next = NULL;
@@ -152,6 +153,32 @@ void packetHandler(u_char *args, const struct pcap_pkthdr *header, const u_char 
 					}
 				}
 			}
+		}*/
+
+		if (connect(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0){
+			 printf("connection error\n");
+		}else{
+			printf("connected\n");
+			char message[6];
+
+			//message[0] = hdr->sa[0];
+			//printf("%02x:%02x:%02x\n", message[0], message[1],message[2]);
+			//struct device *new_dev = (struct device *) malloc (sizeof(struct device));
+			//new_dev->address[0] = hdr->sa[0];
+			//FILE *fd = fdopen(socket_desc, "w");
+			sprintf(message,"%02x:%02x:%02x:%02x:%02x:%02x \n", hdr->sa[0],
+                	                                                    hdr->sa[1],
+                        	                                            hdr->sa[2],
+                                	                                    hdr->sa[3],
+                                        	                            hdr->sa[4],
+                                                	                    hdr->sa[5]);
+			int sockerr = send(socket_desc, message, strlen(message) , 0);
+			if (sockerr >= 0){
+
+			 	printf("%s sended\n", message);
+			}else{
+				printf("socket error %d -> %s\n", sockerr, message);
+			}
 		}
 
 
@@ -164,7 +191,8 @@ void packetHandler(u_char *args, const struct pcap_pkthdr *header, const u_char 
                 struct data_header_t *hdr = (struct data_header_t *) (packet + rthdr->it_len);
 
         }
-
-	printf("Dispositivos encontrados: %d\r", sniffed_devices);
+	close(socket_desc);
+	//sleep(0.5);
+	//printf("Dispositivos encontrados: %d\r", sniffed_devices);
 
 }
